c1f8ffc410bb7f00092e46ec84366f605811cb97
Wolfyysss <joserraguti03@gmail.com>
2025-01-08 20:10:12 +0100

    paquetes

---
+++ ActividadUltima/src/main/java/com/mycompany/actividadultima/Fibonacci_Series.java
@@ -0,0 +1,18 @@
+package com.mycompany.actividadultima;
+
+import java.util.Scanner;
+public class Fibonacci_Series {
+	public static void main(String[] args) {
+		int a = 0, b = 1;
+		int c=0;
+		System.out.println("Enter number : ");
+		Scanner scan = new Scanner(System.in);
+		int i =0;
+		int x = scan.nextInt();
+
+		while (c<x) {
+			System.out.println(c);
+			c = a + b; a = b; b = c;
+			}
+	}
+}
